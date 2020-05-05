9f07f2c2a5245ee379aa7478942f245125db03da
ImpureSilber11 <take1931032@gmail.com>
2020-05-05 21:02:55 +0900

    update

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,7 +16,7 @@ Tag.destroy_all
 nagayan = Entertainer.find_or_create_by(id: 1)
 nagayan.name = 'ながやん'
 nagayan.save!
-nagayan.images.find_or_create_by(id: 1, path: '1', title: '俺は就職活動中の精子！夢はオーマン・コーポレーションに入ることさ！だけどほとんどはスコッティとかいうブラック企業に使われて死んでいくらしいんだ。', file_type: 'jpg')
-nagayan.images.find_or_create_by(id: 2, path: '2', title: 'オーマン・コーポレーションに入社できても、選ばれるのは一人だけさ。他はみんな解雇されちまうんだ。', file_type: 'jpg')
-nagayan.images.find_or_create_by(id: 3, path: '3', title: '帰って晩飯食うかシコルかどっちかの時間しかねえなって思って飯食いながらシコった末、ごはんに欲情するようになった', file_type: 'jpg')
-# nagayan.images.create(path: '4', title: 'ながやん「えーっ。それはイヤかも。わたし人間さんとエッチしたいよ〜っ」', file_type: 'jpg')
+# nagayan.images.find_or_create_by(id: 1, path: '1', title: '俺は就職活動中の精子！夢はオーマン・コーポレーションに入ることさ！だけどほとんどはスコッティとかいうブラック企業に使われて死んでいくらしいんだ。', file_type: 'jpg')
+# nagayan.images.find_or_create_by(id: 2, path: '2', title: 'オーマン・コーポレーションに入社できても、選ばれるのは一人だけさ。他はみんな解雇されちまうんだ。', file_type: 'jpg')
+# nagayan.images.find_or_create_by(id: 3, path: '3', title: '帰って晩飯食うかシコルかどっちかの時間しかねえなって思って飯食いながらシコった末、ごはんに欲情するようになった', file_type: 'jpg')
+# # nagayan.images.create(path: '4', title: 'ながやん「えーっ。それはイヤかも。わたし人間さんとエッチしたいよ〜っ」', file_type: 'jpg')
