a8ca2bc8b013c1a66743a3e2c19d52352073301a
ImpureSilber11 <take1931032@gmail.com>
2020-05-09 18:44:06 +0900

    update

--- app/controllers/entertainers_controller.rb
+++ app/controllers/entertainers_controller.rb
@@ -2,12 +2,28 @@
 
 class EntertainersController < ApplicationController
   def show
-    @entertainer = Entertainer.find(params.permit(:id)[:id])
+    build_params = show_params(params)
+    @entertainer = Entertainer.find(build_params[:id])
     render json: @entertainer
   end
 
   def index
     @entertainers = Entertainer.all
     render json: @entertainers
+  end
+
+  def create
+    build_params = create_params(params)
+    Entertainer.create!(name: build_params[:name])
+  end
+
+  private
+
+  def show_params(params)
+    params.permit(:id)
+  end
+
+  def create_params(params)
+    params.permit(:name)
   end
 end
